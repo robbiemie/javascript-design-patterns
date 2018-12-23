a2ea37d9c2deac9e79b7de1aa896c49d5b95ebe1
yangoogle <yang657850144@gmail.com>
2018-12-23 19:26:17 +0800

    feat: 使用class搭建jquery框架

---
+++ src/01/index.js
@@ -0,0 +1,8 @@
+import jquery from './jquery' // eslint-disable-line
+class Part1 {
+  init () {
+    console.log('jquery(p)', $('p'))
+  }
+}
+
+export default new Part1()
--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-/**
- *
- * @param {*} ins 传入的任意类型
- */
-const say = function (ins) {
-  ins.speak()
-}
-class Dog {
-  speak () {
-    console.log('wowowow')
-  }
-}
-class Duck {
-  speak () {
-    console.log('gagaga')
-  }
-}
+import part1 from './01'
 
-say(new Dog())
-say(new Duck())
+part1.init()
